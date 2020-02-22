99b00908b42631560d3b75bb9df94eebfd25c82a
Christopher Jones <chris.jones@wessexlabels.co.uk>
2020-02-22 08:29:12 +0000

    Base MVC core website, login page, cookie authentication

---
+++ src/Website/Controllers/AuthenticationController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Website.Models;
+
+namespace Website.Controllers
+{
+   public class AuthenticationController : Controller
+   {
+      private readonly ILogger _logger;
+
+      public AuthenticationController(ILogger<AuthenticationController> logger)
+      {
+         _logger = logger;
+      }
+
+      public IActionResult Index()
+      {
+         return View(new LoginViewModel());
+      }
+
+      public async Task<IActionResult> LoginAsync(LoginViewModel login, CancellationToken cancellationToken)
+      {
+         if (!ModelState.IsValid)
+         {
+            return View(new LoginViewModel() { Username = login.Username, RememberMe = login.RememberMe });
+         }
+
+         var claims = new List<Claim>
+         {
+             new Claim(ClaimTypes.Name, login.Username),
+             new Claim("FullName", "Mr Fake User"),
+             new Claim(ClaimTypes.Role, "Administrator"),
+         };
+
+         var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+         var authProperties = new AuthenticationProperties
+         {
+            // 60 minute login
+            ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(60),
+
+            // Persist the cookie after the browser closes
+            IsPersistent = login.RememberMe,
+
+            IssuedUtc = DateTime.UtcNow,
+
+            RedirectUri = "/"
+         };
+
+         await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                                       new ClaimsPrincipal(claimsIdentity),
+                                       authProperties);
+
+         return RedirectToActionPermanent("Index", "Home");
+      }
+   }
+}
---
+++ src/Website/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Website.Models
+{
+   public class LoginViewModel
+   {
+      [Required]
+      public string Username { get;set;} = "";
+
+      public string Password { get;set;} = "";
+
+      public bool RememberMe { get;set;} = false;
+   }
+}
